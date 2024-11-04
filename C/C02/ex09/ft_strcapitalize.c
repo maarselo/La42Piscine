45bb8ea915112ad2825ed2eac882ea6de45e68ee
Marcelo Villavicencio Percca <mvillavi@cbr12s4.42barcelona.com>
2024-11-04 18:36:13 +0100

    Youuu

--- C/C02/ex09/ft_strcapitalize.c
+++ C/C02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: mvillavi <mvillavi@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 11:45:36 by mvillavi          #+#    #+#             */
-/*   Updated: 2024/11/02 12:40:09 by mvillavi         ###   ########.fr       */
+/*   Updated: 2024/11/04 18:03:32 by mvillavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,16 @@ char	*ft_strcapitalize(char *str)
 	int	i;
 
 	i = 1;
-	
 	if (97 <= str[0] && str[0] <= 122)
 		str[0] = str[0] - 32;
 	while (str[i] != '\0')
 	{
 		if (97 <= str[i] && str[i] <= 122)
 		{
-		 	if((65 <= str[i-1] && str[i-1] <= 90) 
-				|| (48 <= str[i-1] && str[i-1] <= 57)
-				|| (97 <= str[i-1] && str[i-1] <=122))
+			if ((65 <= str[i - 1] && str[i - 1] <= 90) || \
+			(48 <= str[i - 1] && str[i - 1] <= 57) || \
+			(97 <= str[i - 1] && str[i - 1] <= 122))
 				str[i] = str[i];
-				
 			else
 				str[i] = str[i] - 32;
 		}
@@ -39,8 +37,8 @@ char	*ft_strcapitalize(char *str)
 
 int	main(void)
 {
-	char	str[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
-	
+	char	str[] = "salut, comment tu vas ? 42mots quarante-deux";
+
 	printf("%s\n", ft_strcapitalize(str));
 	return (0);
 }
