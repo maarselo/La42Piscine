ec0f3ad2c709d2173d583ab6785eb4bc00825942
Marcelo Villavicencio Percca <mvillavi@cbr15s6.42barcelona.com>
2024-11-08 07:27:17 +0100

    Elpepe

---
+++ C/C04/ex00/ft_strlen.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvillavi <mvillavi@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 21:19:52 by mvillavi          #+#    #+#             */
+/*   Updated: 2024/11/08 07:12:06 by mvillavi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	str[] = "Hola";
+
+	printf("%d", ft_strlen(str));
+	return (0);
+}
+*/
---
+++ C/C04/ex02/ft_putnbr.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvillavi <mvillavi@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 01:25:23 by mvillavi          #+#    #+#             */
+/*   Updated: 2024/11/08 07:10:23 by mvillavi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
+	if (nb < 10)
+	{
+		ft_putchar(nb % 10 + '0');
+	}
+}
+/*
+int	main(void)
+{
+	int	n;
+
+	n = 18;
+	ft_putnbr(n);
+	return (0);
+}
+*/
